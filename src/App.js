85446908082b93e2d729247ba0d85d4e3a1c99c0
bbott0 <leam_e@naver.com>
2024-09-24 19:15:53 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './components/Main/Main';
-
+import Header from './components/App/Header';
 function App() {
   return (
     <div className="App">
     	<Router>
+        <Header/>
 	  	  <Routes>
-	      	<Route path = "/home" element={<Main/>}/>
+	      	<Route path = "/" element={<Main/>}/>
 	      </Routes>
       </Router>
     </div>
---
+++ src/components/App/Header.js
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header(){
+    
+    return(
+        <>
+           <div className = "headerContainer">
+            <a>header</a>
+           </div>
+        </>
+    );
+};
+export default Header
